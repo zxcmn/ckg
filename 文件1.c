72bf24b29d5769236c04472de2efcd47c996b0cc
zxcmn <51371035+zxcmn@users.noreply.github.com>
2022-04-17 13:08:23 +0800

    Update 文件1.c

--- 文件1.c
+++ 文件1.c
@@ -359,6 +359,50 @@
 
 
 
+//打印菱形图案
+
+
+
+
+//int main() {
+//
+//	int line;
+//	scanf("%d", &line);
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < line; i++) {//上半部分
+//		
+//		for (j = 0; j < line - 1 - i; j++) {//line-1=6
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * i + 1; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < line-1; i++) {//下半部分
+//		for (j = 0; j <=i; j++) {
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2*(line-1-i)-1; j++) {//2*6-1；2*5-1;2*4-1.....line-1=6
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
