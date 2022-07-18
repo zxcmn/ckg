a3269d0dd83ba9713df5650447bb28b4c1babf30
zxcmn <51371035+zxcmn@users.noreply.github.com>
2022-07-18 12:49:57 +0800

    Update SeqList.h

--- DataStruct/SeqList.h
+++ DataStruct/SeqList.h
@@ -14,7 +14,8 @@ typedef struct SeqList
 void SeqListInit(SL* ps);//结构体初始化
 void SeqListPushBack(SL* ps, SLDataType x);//结构体尾添
 void SeqListPopBack(SL* ps);//结构体尾删
-void SeqListPushFront(SL* Ps, SLDataType x);
-void SeqLIstPopFront(SL* ps);
+void SeqListPushFront(SL* Ps, SLDataType x);//头插
+void SeqListPushFront2(SL* Ps, SLDataType x);//三步头插
+void SeqLIstPopFront(SL* ps);//头删
 void SeqListPrint(SL* ps);
 void SeqListDestory(SL* ps);
