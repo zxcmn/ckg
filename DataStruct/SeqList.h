a64fd0799a50621fb599ff9142ee71f0817c424a
zxcmn <51371035+zxcmn@users.noreply.github.com>
2022-07-18 21:00:29 +0800

    Update SeqList.h

--- DataStruct/SeqList.h
+++ DataStruct/SeqList.h
@@ -14,8 +14,13 @@ typedef struct SeqList
 void SeqListInit(SL* ps);//结构体初始化
 void SeqListPushBack(SL* ps, SLDataType x);//结构体尾添
 void SeqListPopBack(SL* ps);//结构体尾删
-void SeqListPushFront(SL* Ps, SLDataType x);//头插
+void SeqListPushFront(SL* Ps, SLDataType x);//移动头插
+void SeqListPopFront(SL* ps);//移动头删
 void SeqListPushFront2(SL* Ps, SLDataType x);//三步头插
-void SeqLIstPopFront(SL* ps);//头删
-void SeqListPrint(SL* ps);
-void SeqListDestory(SL* ps);
+void SeqListPopFront3(SL* ps);//三步头删
+void SeqListPrint(SL* ps);//打印
+void SeqListDestory(SL* ps);//释放空间
+void SeqListCheckCapacity(SL* ps);//check检查，使用多次的扩容功能，封装成函数
+int SeqListFind(SL* ps,SLDataType x);//查找
+void SeqListInsert(SL* ps,size_t pos,SLDataType x);
+void SeqListDelete(SL* ps,size_t pos);
