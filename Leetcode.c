1c6d7010060693b859b9291b8e1201a83d94ee58
zxcmn <51371035+zxcmn@users.noreply.github.com>
2022-07-25 20:15:00 +0800

    Update Leetcode.c

--- Leetcode.c
+++ Leetcode.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1    
 
-															//1.【删除有序数组中的重复项】
+															//1.【26删除有序数组中的重复项】
 
 //思路：双指针，如果指针2不等于指针1，指针1++，赋值为*指针2
 //int removeDuplicates(int* nums, int numsSize) {
@@ -19,7 +19,7 @@
 //}
 
 
-															//2.【移除元素】
+															//2.【27移除元素】
 
 //双指针，两个人，你去找，我定位，找到不符合放过来，我定位下一个
 //用下标
@@ -57,3 +57,28 @@
 
 
 
+																//3.【88合并俩个有序数组】
+
+//思路：从后往前归并，同时考虑223 116 这种223指针先减完的情况
+//void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+//    int temp = nums1Size - 1;
+//    while (temp >= 0 && n > 0)
+//    {
+//        if (m <= 0)
+//        {
+//            nums1[temp] = nums2[n - 1];
+//            --n;
+//        }
+//        else if (nums1[m - 1] <= nums2[n - 1])
+//        {
+//            nums1[temp] = nums2[n - 1];
+//            --n;
+//        }
+//        else
+//        {
+//            nums1[temp] = nums1[m - 1];
+//            --m;
+//        }
+//        --temp;
+//    }
+//}//时间复杂度O(m+n);
