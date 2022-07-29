8e90cf8f3aeb90a8ddbef867a39a8c518b149caf
zxcmn <51371035+zxcmn@users.noreply.github.com>
2022-07-29 23:04:37 +0800

    Update Test.c

--- DataStruct/Test.c
+++ DataStruct/Test.c
@@ -1,145 +1,126 @@
 #define _CRT_SECURE_NO_WARNINGS 1    
 
+#include"SingleLink.h"
 
-#include"SeqList.h"
 
-void TestSeqList1()
+
+
+
+
+
+void Test1()
 {
-	SL sl;
-	SeqListInit(&sl);//初始化
-	SeqListPushBack(&sl, 1);
-	SeqListPushBack(&sl, 2);
-	SeqListPushBack(&sl, 3);
-	SeqListPushBack(&sl, 4);
-	SeqListPushBack(&sl, 5);
-	SeqListPopBack(&sl);//尾删
-	SeqListPopBack(&sl);//尾删
-	SeqListPopBack(&sl);//尾删
+	SL* phead = NULL;
+	SeqListPushBack(&phead,1);
+	SeqListPushBack(&phead,2);
+	SeqListPushBack(&phead,3);
+	SeqListPushBack(&phead,4);
 
-	SeqListPrint(&sl);//打印
-	SeqListDestory(&sl);//释放空间
+	SeqListPrint(phead);
+}	
+
+void Test2()
+{
+	SL* phead = NULL;
+	SeqListPushFront(&phead,1);
+	SeqListPushFront(&phead,2);
+	SeqListPushFront(&phead,3);
+	SeqListPushFront(&phead,4);
+	SeqListPrint(phead);
 }
 
-//移动首插
-void TestSeqList2()
+void Test3()
 {
-	SL sl;
-	SeqListInit(&sl);//初始化
-	SeqListPushBack(&sl, 1);
-	SeqListPushBack(&sl, 2);
-	SeqListPushBack(&sl, 3);
-	SeqListPushBack(&sl, 4);
-	SeqListPushBack(&sl, 5);
-	SeqListPushFront(&sl,10);//首插入
-	SeqListPushFront(&sl,20);//首插入
-	SeqListPushFront(&sl,30);//首插入
-	SeqListPushFront(&sl,40);//首插入
-	SeqListPushFront(&sl,50);//首插入
+	SL* pphead = NULL;
+	SeqListPushFront(&pphead, 1);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 3);
+	SeqListPushFront(&pphead, 4);
+	SeqListPrint(pphead);
+	SeqListPopDelete(&pphead);
+	SeqListPopDelete(&pphead);
 
-	SeqListPopFront(&sl);//头删
-	SeqListPopFront(&sl);//头删
+	SeqListPrint(pphead);
+
+}
+
+void Test4()
+{
+	SL* pphead = NULL;
+	SeqListPushFront(&pphead, 1);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 3);
+	SeqListPushFront(&pphead, 4);
+	SeqListPrint(pphead);
+	SeqListHeadDelete(&pphead);
+	SeqListHeadDelete(&pphead);
+	SeqListPrint(pphead);
+
+}
+
+void Test5()
+{
+	SL* pphead = NULL;
+	SeqListPushFront(&pphead, 1);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 3);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 4);
+	SeqListPrint(pphead);
+	int i = 1;
+	SL* pos = SeqListFind(pphead, 2);
+	while (pos)
+	{
+		printf("第%d个pos节点：%p->%d\n", i++, pos,pos->data);
+		pos = SeqListFind(pos->next,2);
+	}
+}
+
+void Test6()
+{
+	SL* pphead = NULL;
+	SeqListPushFront(&pphead, 1);
+	SeqListPushFront(&pphead, 2);
+	SeqListPushFront(&pphead, 3);
+	SeqListPushFront(&pphead, 4);
 	
-	SeqListPrint(&sl);//打印
-	SeqListDestory(&sl);//释放空间
-
-}
-
-
-//三步首添+首删
-void TestSeqList3()
-{
-	SL sl;
-	SeqListInit(&sl);//初始化
-	SeqListPushBack(&sl, 1);
-	SeqListPushBack(&sl, 2);
-	SeqListPushBack(&sl, 3);
-	SeqListPushBack(&sl, 4);
-	SeqListPushBack(&sl, 5);
-	SeqListPushFront2(&sl, 10);//首插入10
-	SeqListPushFront2(&sl, 20);//首插入10
-	SeqListPushFront2(&sl, 30);//首插入10
-	SeqListPushFront2(&sl, 40);//首插入10
-	SeqListPushFront2(&sl, 50);//首插入10
-
-	SeqListPopFront3(&sl);
-	SeqListPopFront3(&sl);
 	
-	SeqListPrint(&sl);//打印
-	SeqListDestory(&sl);//销毁空间
-}
-
-void TestSeqList4()
-{
-	SL sl;
-	SeqListInit(&sl);//初始化
-	SeqListPushBack(&sl, 1);
-	SeqListPushBack(&sl, 2);
-	SeqListPushBack(&sl, 3);
-	SeqListPushBack(&sl, 4);
-	SeqListPushBack(&sl, 5);
-	SeqListPushFront(&sl, 10);//首插入
-	SeqListPushFront(&sl, 20);//首插入
-	SeqListPushFront(&sl, 30);//首插入
-	SeqListPushFront(&sl, 40);//首插入
-	SeqListPushFront(&sl, 50);//首插入
-
-	SeqListPopFront(&sl);//头删
-	SeqListPopFront(&sl);//头删
-	int temp = 0;
-	temp = SeqListFind(&sl,3);//查找
-	if (temp == -1)
-		 printf("没找到\n");
-	else 
-		printf("找到了，下标 = %d\n", temp);
-
-
-	SeqListPrint(&sl);//打印
-	SeqListDestory(&sl);//释放空间
-
-}
-
-void TestSeqList5()
-{
-	SL sl;
-	SeqListInit(&sl);//初始化
-	SeqListPushBack(&sl, 1);
-	SeqListPushBack(&sl, 2);
-	SeqListPushBack(&sl, 3);
-	SeqListPushBack(&sl, 4);
-	SeqListPushBack(&sl, 5);
+	SeqListPrint(pphead);
+	;
 	
-	SeqListInsert(&sl,3,99);//插入(结构体，插入的下标，插入数)
-	SeqListDelete(&sl,4);//删除指定下标
-
-	SeqListPrint(&sl);//打印
-	SeqListDestory(&sl);//释放空间
-
+	SeqListInsert(&pphead, SeqListFind(pphead, 2), 30);
+	SeqListInsert(&pphead, SeqListFind(pphead, 4), 30);
+	SeqListPrint(pphead);
+	//如果只有一个节点，又有问题，太繁琐，并且付出一定的效率代价（找前一个）不建议节点前插
 }
 
 int main()
 {
-	
-	
-	//TestSeqList1();//尾添尾删
-	//TestSeqList2();//首添首删
-	//TestSeqList3();//使用三步翻转实现首添
-	//TestSeqList4();//查找数
-	TestSeqList5();//插入
-
-
-	//SeqListPushFront(SL * Ps, SLDataType x);//头插
-	//SeqLIstPopFront(SL * ps);//头删
-
-
-
-
-
-
-
-	
-
+	//Test1();//尾插
+	//Test2();//首插
+	//Test3();//尾删
+	//Test4();//首删
+	//Test5();//查找位置
+	Test6();//节点前插入/太繁琐/效率代价，不建议在前面插入
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
